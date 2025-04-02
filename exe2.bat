3671fb630c26c864148ef717f372351e4009090a
JaydenNguyen1211 <nqhuy.vn95@gmail.com>
2025-04-03 01:34:39 +0700

    Added image

--- exe2.bat
+++ exe2.bat
@@ -4,16 +4,29 @@ echo Starting
 :: Set GitHub repo URL and image URL
 set GIT_REPO=https://github.com/JaydenNguyen1211/BatMan.git
 set IMAGE_URL=https://img.freepik.com/free-photo/sexy-blond-female-cream-dress-posing-big-hall_613910-2851.jpg
-set IMAGE_NAME=blonde.jpg
-
-git init
-git remote add origin https://github.com/JaydenNguyen1211/BatMan.git
+set IMAGE_NAME=blonde6.jpg
 
 
-curl -o IMAGE_NAME IMAGE_URL
+curl -o %IMAGE_NAME% %IMAGE_URL%
 
+echo Waiting for downloading IMAGE_NAME
+timeout /t 10 /nobreak >nul
+
+if not exist IMAGE_NAME (
+	echo Image download failed. Exiting...
+)
+
+echo Image downloaded successfully.
+
+:: Add all files to Git
 git add .
-git commit -m "Start init"
 
-git push origin master
+:: Commit changes
+git commit -m "Added image"
+
+:: Push changes to GitHub
+git push -u origin main
+
+echo Done!
+pause
 
